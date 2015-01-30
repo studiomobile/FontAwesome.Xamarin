2a10599bba34a2412d26e4364a994b4453eee5b2
Andrey Verbin <andrey.verbin@gmail.com>
2015-01-30 15:12:15 +0300

    Adapt to latest xamarin version

--- FontAwesome.Xamarin/UIKitExtension.cs
+++ FontAwesome.Xamarin/UIKitExtension.cs
@@ -21,8 +21,9 @@ namespace FontAwesome.iOS
 		private static FAImageGenerator generator = new FAImageGenerator();
 		public static void AppendIcon(this UILabel label, string icon)
 		{
-			using (var text = new NSMutableAttributedString ()) {
-				var iconStr = text.FromTextAttachment (label.IconAttachment(icon));
+			using (var text = new NSMutableAttributedString ()) 
+			using (var iconStr = NSAttributedString.CreateFrom (label.IconAttachment(icon)))
+			{
 				text.Append (iconStr);
 				label.AttributedText = text;
 			}
@@ -41,7 +42,7 @@ namespace FontAwesome.iOS
 		{
 			using (var finalText = new NSMutableAttributedString ())
 			using (var iconAttachment = label.IconAttachment (icon))
-			using (var iconText = finalText.FromTextAttachment (iconAttachment))
+			using (var iconText = NSAttributedString.CreateFrom (iconAttachment))
 			using (var labelText = new NSAttributedString (text)) {
 				finalText.Append (iconText);
 				finalText.Append (labelText);
@@ -53,7 +54,7 @@ namespace FontAwesome.iOS
 		{
 			using (var finalText = new NSMutableAttributedString ())
 			using (var iconAttachment = label.IconAttachment (icon))
-			using (var iconText = finalText.FromTextAttachment (iconAttachment))
+			using (var iconText = NSAttributedString.CreateFrom (iconAttachment))
 			using (var labelText = new NSAttributedString (text)) {
 				finalText.Append (labelText);
 				finalText.Append (iconText);
@@ -63,21 +64,34 @@ namespace FontAwesome.iOS
 
 		public static NSTextAttachment IconAttachment(this UILabel label, string icon)
 		{
-			var font = label.Font;
-			var imageSize = font.xHeight + font.CapHeight;
-
-			generator.Size = imageSize;
-			generator.Insets = new UIEdgeInsets (1, 1, 1, 1);
-			generator.Colors = new UIColor[] { label.TextColor };
-
-			using (var text = new NSMutableAttributedString ()) {
-				var iconAttachment = new NSTextAttachment ();
-				iconAttachment.Bounds = new CGRect (new CGPoint (0, font.Descender), new CGSize (imageSize, imageSize));
-				iconAttachment.Image = generator.CreateImage (FAIcons.FAClockO);
-				return iconAttachment;
-			}
+			return IconAttachment (label.Font, label.TextColor, icon);
 		}
 
+		public static NSTextAttachment IconAttachment(UIFont font, UIColor textColor, string icon)
+		{
+			var imageSize = font.xHeight + font.CapHeight;
+			return IconAttachment ((float)imageSize, (float)font.Descender, textColor, icon);
+		}
+
+		public static NSTextAttachment IconAttachment(float imageSize, float yOffset, UIColor textColor, string icon)
+		{
+			generator.Size = imageSize;
+			generator.Insets = new UIEdgeInsets (1, 1, 1, 1);
+			generator.Colors = new UIColor[] { textColor };
+
+			return new NSTextAttachment () { 
+				Bounds = new CGRect (new CGPoint (0, yOffset), new CGSize (imageSize, imageSize)),
+				Image = generator.CreateImage (icon)
+			};
+		}
+
+		public static NSTextAttachment IconAttachment(float imageSize, float yOffset, UIImage image)
+		{
+			return new NSTextAttachment () { 
+				Bounds = new CGRect (new CGPoint (0, yOffset), new CGSize (imageSize, imageSize)),
+				Image = image
+			};
+		}
 
 		public static void SetIcon(this UIButton btn, string icon, nfloat size, UIControlState state = UIControlState.Normal)
 		{
@@ -88,6 +102,11 @@ namespace FontAwesome.iOS
 		public static void SetIcon(this UIButton btn, string icon, UIControlState state = UIControlState.Normal)
 		{
 			SetIcon (btn, icon, btn.Font.PointSize, state);
+		}
+
+		public static NSTextAttachment IconAttachment(this UIButton btn, UIControlState state, string icon)
+		{
+			return IconAttachment (btn.Font, btn.TitleColor(state), icon);
 		}
 
 		public static void SetIcon(this UIBarButtonItem btn, string icon, UIColor color, nfloat size)
